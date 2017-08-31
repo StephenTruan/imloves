80e3e95a0f7becbdcf721b9d32c69378073bbf2f
wujianchuan <wujianchuan@shinow.com>
2017-08-31 22:15:07 +0800

    修改sql文件。

--- imloves.sql
+++ imloves.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-08-29 16:59:46
+Date: 2017-08-31 22:11:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,8 +28,8 @@ CREATE TABLE `sys_role` (
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
-INSERT INTO `sys_role` VALUES ('1', 'ROLE_ADMIN');
-INSERT INTO `sys_role` VALUES ('2', 'ROLE_USER');
+INSERT INTO `sys_role` VALUES ('1', 'ROLE_USER');
+INSERT INTO `sys_role` VALUES ('2', 'ROLE_ADMIN');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -48,13 +48,11 @@ CREATE TABLE `sys_user` (
   `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'ASDFGHJKL', 'jack', '000000', '18231926271', '0', '1', '邢台', '2017-08-28 08:23:27', '2017-08-28 08:23:27');
-INSERT INTO `sys_user` VALUES ('2', null, 'wu', '$2a$10$oR/87phv.1f7y6whdisGZ.qJMF3DjZ05epxegsSHbPz/F/nX2g9RS', null, null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -66,12 +64,17 @@ CREATE TABLE `sys_user_role` (
   `ROLE_ID` int(11) NOT NULL COMMENT '角色数据编号',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `USER_ID` (`USER_ID`,`ROLE_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
 INSERT INTO `sys_user_role` VALUES ('2', '1', '1');
 INSERT INTO `sys_user_role` VALUES ('1', '1', '2');
-INSERT INTO `sys_user_role` VALUES ('3', '2', '1');
-INSERT INTO `sys_user_role` VALUES ('4', '2', '2');
+INSERT INTO `sys_user_role` VALUES ('4', '2', '1');
+INSERT INTO `sys_user_role` VALUES ('5', '3', '1');
+INSERT INTO `sys_user_role` VALUES ('6', '4', '1');
+INSERT INTO `sys_user_role` VALUES ('7', '5', '1');
+INSERT INTO `sys_user_role` VALUES ('8', '6', '1');
+INSERT INTO `sys_user_role` VALUES ('9', '7', '1');
+INSERT INTO `sys_user_role` VALUES ('10', '8', '1');
